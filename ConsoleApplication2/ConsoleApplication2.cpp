0e58d2c2384f94ec9f5e022e0c78cd6c4f032de2
A. <Alekseew.zheka0207@gmail.com>
2024-11-16 22:53:42 +0200

    improved

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,58 +6,126 @@
 #include <sstream>
 #include <ctype.h>
 
-
 using namespace std;
 namespace fs = filesystem;
 
+bool checking_symbol_of_punctual(char sym) {
+	if (sym == ' ' || sym == ',' || sym == '.' || sym == ';' || sym == ':' || sym == '[' || sym == ']' || sym == '{' || sym == '}' || sym == '`' || sym == '~' || sym == '@' || sym == '#' || sym == '#' || sym == '#' || sym == '$' || sym == '%' || sym == '^' || sym == '&' || sym == '*' || sym == '(' || sym == ')' || sym == '\\' || sym == '|' || sym == '/' || sym == '=' || sym == '-' || sym == '+' || sym == '<' || sym == '>' || sym == '!' || sym == '?') {
+		return true;
+	}
+	return false;
+}
 
+void prepearing_word(string& word) {
+	int index{ 0 };
+	for (int i = 0; i < word.size(); i++) {
+		if (checking_symbol_of_punctual(word[i])) {
+			word.erase(index);
+			continue;
+		}
+		index++;
+	}
+}
 
 int main(){
 	SetConsoleOutputCP(1251);
-	vector<string> array_of_words;//keys_for_dict
 
-	map<string, int> word_count;
-	
-	fs::path path_to_essay = "essay of reflection.txt";
+	ofstream for_write("datas.txt");
 
-	stringstream ssfile;
-	ifstream file_read(path_to_essay);
+	string choice{ 0 };
+	cout << "You want write youself text? 1(yes) 2(no): \n";
+	cin >> choice;
+	map<string, int> datas;
 
-	string word;
+	if (choice != "1") {
+		map<string, int> word_count;
+		fs::path path_to_essay = "essay of reflection.txt";
+		stringstream ssfile;
+		ifstream file_read(path_to_essay);
+		string word;
 
-	if (!file_read.is_open()) {
-		return 1;
+		if (!file_read.is_open()) {
+			return 1;
+		}
+
+		ssfile << file_read.rdbuf();
+		file_read.close();
+
+		while (ssfile >> word) {
+			cout << word + ' ';
+		}
+		ssfile.clear();
+		ssfile.seekg(0);
+
+		while (ssfile >> word) {
+			prepearing_word(word);
+			word_count[word]++;
+		}
+
+		string most_used;
+		int count_of_used{ 0 };
+
+		for (const auto& pair : word_count) {
+			if (count_of_used <= pair.second) {
+				count_of_used = pair.second;
+				most_used = pair.first;
+			}
+			cout << "Word: " << pair.first << " : " << pair.second << endl;
+		}
+		cout << "Most used word is: " << most_used << " times: " << count_of_used;
+		datas[most_used] = count_of_used;
+	}
+	else {
+		stringstream ssfile;
+		map<string, int> word_count;
+		string sentence;
+
+		cout << "Write your sentence here: \n";
+		cin.ignore();
+		getline(cin, sentence);
+		
+		cout << endl;
+
+		string buffer{};
+		for (int i = 0; i < sentence.size(); i++) {
+			if (checking_symbol_of_punctual(tolower(sentence[i])) && !buffer.empty()) {
+				for (int j = 0; j < buffer.size(); j++) {
+					buffer[j] = tolower(buffer[j]);
+				}
+				word_count[buffer]++;
+				buffer.clear();
+				continue;
+			}
+			if (checking_symbol_of_punctual(sentence[i])) {
+				continue;
+			}
+			buffer += sentence[i];
+		}
+		if (!buffer.empty()) {
+			word_count[buffer]++;
+		}
+
+
+		string most_used;
+		int count_of_used{ 0 };
+
+		cout << endl;
+
+		for (const auto& pair : word_count) {
+			if (count_of_used <= pair.second) {
+				count_of_used = pair.second;
+				most_used = pair.first;
+			}
+			cout << "Word: " << pair.first << " : " << pair.second << endl;
+		}
+		cout << "Most used word is: " << most_used << " times: " << count_of_used;
+
+		datas[most_used] = count_of_used;
+
 	}
 
-	ssfile << file_read.rdbuf();
-	file_read.close();
-
-	while (ssfile >> word) {
-
-		int index{ 0 };
-		//DOESN'T WORKING WITH UKRAINIAN LANGUAGE :(
-		//while(index < word.size()) {
-
-		//	if (!isalpha(word[index], locale("uk_UA.UTF-8"))) {
-		//		word.erase(index);
-		//	}
-		//	else {
-		//		index++;
-		//	}
-
-		//}
-		array_of_words.push_back(word); 
-		cout << word + ' ';
-	}
-	ssfile.clear();
-	ssfile.seekg(0);
-
-	while (ssfile >> word) {
-		word_count[word]++;
-	}
-
-	for (const auto& pair : word_count) {
-		cout << "Word: " << pair.first << " : " << pair.second << endl;
+	for (const auto& pair : datas) {
+		for_write << "Key: " + pair.first + " Value: " + to_string(pair.second) + '\n';
 	}
 
 
